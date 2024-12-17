fa511b90f1bdaea6783d5d043dae6bf0946408c7
sankalp0607 <sankalpshukla.madwas@gmail.com>
2024-12-17 15:21:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Sankalp IT Teaching Hub Portfolio Loaded!");
+    
+    
+    const contactForm = document.querySelector('.contact form');
+    
+    contactForm.addEventListener('submit', (event) => {
+        event.preventDefault(); 
+        alert('Thank you for your message! We will get back to you soon.');
+        contactForm.reset();  
+    });
+});
